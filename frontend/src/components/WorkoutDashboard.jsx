03e466175090f62106169c87c1c089992d9d5582
domc13 <119551389+domc13@users.noreply.github.com>
2024-07-19 09:39:23 -0400

    created workout dashboard for summarized data

--- frontend/src/components/WorkoutDashboard.jsx
+++ frontend/src/components/WorkoutDashboard.jsx
@@ -1,7 +1,19 @@
-import { useState, useEffect } from "react";
+import React from 'react';
+import "../styles/WorkoutDashboard.css";
 
-function WorkoutDashboard() {
-  
+function WorkoutDashboard({ totalCalories, totalTime }) {
+  return (
+    <div className="workout-dashboard">
+      <div className="dashboard-item">
+        <h3>Total Calories Burned</h3>
+        <p>{totalCalories}</p>
+      </div>
+      <div className="dashboard-item">
+        <h3>Total Workout Duration (minutes)</h3>
+        <p>{totalTime}</p>
+      </div>
+    </div>
+  );
 }
 
 export default WorkoutDashboard;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,16 +2,23 @@ import { useState, useEffect } from "react"
 import api from "../api"
 import Workout from "../components/Workout"
 import "../styles/Home.css"
+import WorkoutDashboard from "../components/WorkoutDashboard"
 
 function Home() {
   const [workouts, setWorkouts] = useState([]);
   const [exerciseName, setExerciseName] = useState("");
   const [calories, setCalories] = useState(0);
   const [timeWorkedOut, setTimeWorkedOut] = useState(0);
+  const [totalCalories, setTotalCalories] = useState(0);
+  const [totalTime, setTotalTime] = useState(0);
 
   useEffect(() => {
     getWorkouts();
   }, [])
+
+  useEffect(() => {
+    calculateTotals();
+  }, [workouts]);
 
   const getWorkouts = () => {
     api
@@ -45,7 +52,15 @@ function Home() {
       .catch((err) => alert(err))
   }
 
+  const calculateTotals = () => {
+    const totalCals = workouts.reduce((acc, workout) => acc + workout.calories, 0);
+    const totalTime = workouts.reduce((acc, workout) => acc + workout.time_worked_out, 0);
+    setTotalCalories(totalCals);
+    setTotalTime(totalTime);
+  };
+
   return <div>
+    <WorkoutDashboard totalCalories={totalCalories} totalTime={totalTime} />
     <form onSubmit={createWorkout}>
       <label htmlFor="exerciseName">Exercise Name:</label>
       <br />
@@ -83,7 +98,7 @@ function Home() {
       <input type="submit" value="Submit" />
     </form>
     <div>
-      <h2>Workout Log</h2>
+      {/* <h2 >Workout Log</h2> */}
       <div className="workout-log-container">
         <div className="workout-log-container-header">
           <h3>Exercise Name</h3>
